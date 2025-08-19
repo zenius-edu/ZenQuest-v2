0c00222d0870a1b8f8ba1394e2ef5cd6005f6b83
bagasprima <bagas.prima@zenius.com>
2025-08-19 13:00:38 +0700

    feat(quest): add samples (#8)

--- BE/app/src/app/quest/routes.clj
+++ BE/app/src/app/quest/routes.clj
@@ -6,6 +6,8 @@
 (defn api-routes
   [db openai midware]
   ["/quest"
+   ["/list" {:get (partial midware quest/list-quests db openai)}]
    ["/generate" {:post (partial midware quest/generate-quest db openai)}] 
    ["/generate-audio" {:post (partial midware quest/generate-audio db openai)}]
-   ["/start" {:post (partial midware quest/start-session db openai)}]])
+   ["/start" {:post (partial midware quest/start-session db openai)}]
+   ["/submit" {:post (partial midware quest/submit-answers db openai)}]])
